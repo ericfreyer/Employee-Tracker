8833972a30c702ec9edd22c13cb4e32115ddf5c9
ericfreyer <72565719+ericfreyer@users.noreply.github.com>
2021-02-07 11:50:10 -0500

    added .env to hide password, and server properly connecting

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,2 +1,20 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer")
+const cTable = require("console.table");
+const password = require("./.env")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "employeeTracker_db"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+  });
+
+
+
